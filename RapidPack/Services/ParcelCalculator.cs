f7ba9902e8ee64a585fb7e4ff97f2644cbe4302d
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-03-09 20:37:21 +0100

    Add basic ParcelCalculator class

--- RapidPack/Services/ParcelCalculator.cs
+++ RapidPack/Services/ParcelCalculator.cs
@@ -2,5 +2,19 @@
 
 public class ParcelCalculator
 {
-    
+    const int cenaBazowa = 10;
+    const int cenaZaKilogram = 2;
+    const int maxWaga = 30;
+
+    int CalculatePrice(int waga)
+    {
+        if (waga > maxWaga)
+        {
+            
+        }
+
+        int cena = cenaBazowa + (waga*cenaZaKilogram);
+
+        return cena;
+    }
 }
