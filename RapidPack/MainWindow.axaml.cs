416254f76889d4a36cdcd5bb9ee553958a0c6ea4
jakubg <jakub.gawronski@cyfroweszkoly.pl>
2026-03-13 09:23:05 +0100

    Add eror message when waga>30

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -4,8 +4,21 @@ namespace RapidPack;
 
 public partial class MainWindow : Window
 {
+    private TextBlock _WeightErrorBox;
+    private TextBox _Waga;
     public MainWindow()
     {
         InitializeComponent();
+        _Waga = this.FindControl<TextBox>("WagaTextBox");
+        _WeightErrorBox = this.FindControl<TextBlock>("ShowWeightErrorTextBox");
+
+        _Waga.KeyUp += (_, _) =>
+        {
+            if (int.TryParse(_Waga.Text, out int weight) && weight > 30)
+                _WeightErrorBox.Text = "Waga paczki nie może przekraczać 30kg!";
+            else
+                _WeightErrorBox.Text = "";
+        };
     }
+
 }
