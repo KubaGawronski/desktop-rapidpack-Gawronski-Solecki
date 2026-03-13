6a26b3b37f1d40197f9b63c80ef77111b3d8ffa1
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-03-14 00:33:35 +0100

    Add showing price of parcel and calculating price based on weight

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using RapidPack.Services;
 
 namespace RapidPack;
 
@@ -6,18 +7,29 @@ public partial class MainWindow : Window
 {
     private TextBlock _WeightErrorBox;
     private TextBox _Waga;
+    private TextBlock _Cena;
+    ParcelCalculator _calculator = new ParcelCalculator();
     public MainWindow()
     {
         InitializeComponent();
         _Waga = this.FindControl<TextBox>("WagaTextBox");
         _WeightErrorBox = this.FindControl<TextBlock>("ShowWeightErrorTextBox");
+        _Cena = this.FindControl<TextBlock>("PriceTextBox");
 
         _Waga.KeyUp += (_, _) =>
         {
             if (int.TryParse(_Waga.Text, out int weight) && weight > 30)
+            {
                 _WeightErrorBox.Text = "Waga paczki nie może przekraczać 30kg!";
+                _Cena.Text = "";
+            }
             else
-                _WeightErrorBox.Text = "";
+            {
+              _WeightErrorBox.Text = "";   
+              int price = _calculator.CalculatePrice(weight);
+              _Cena.Text = $"Cena: {price} zł";
+            }
+               
         };
     }
 
