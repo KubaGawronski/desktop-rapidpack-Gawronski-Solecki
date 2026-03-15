f25b9ba7529da48e6c3ebaa2257b1e228b7b8256
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-03-15 15:38:09 +0100

    Add summary and connect button to event

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using RapidPack.Services;
 
 namespace RapidPack;
@@ -14,13 +15,14 @@ public partial class MainWindow : Window
     private CheckBox _Ekspres;
     private ComboBox _TypPrzesylki;
     private Button _SubmitButton;
+    private TextBlock _Podsumowanie;
     ParcelCalculator _calculator = new ParcelCalculator();
     public MainWindow()
     {
         InitializeComponent();
         _Waga = this.FindControl<TextBox>("WagaTextBox");
         _WeightErrorBox = this.FindControl<TextBlock>("ShowWeightErrorTextBox");
-        _Cena = this.FindControl<TextBlock>("PriceTextBox");
+        _Cena = this.FindControl<TextBlock>("PriceTextBlock");
         _Wysokosc = this.FindControl<TextBox>("WysokoscTextBox");
         _Szerokosc = this.FindControl<TextBox>("SzerokoscTextBox");
         _Glebokosc = this.FindControl<TextBox>("GlebokoscTextBox");
@@ -28,6 +30,8 @@ public partial class MainWindow : Window
         _TypPrzesylki = this.FindControl<ComboBox>("TypPrzesylkiComboBox");
         _SubmitButton = this.FindControl<Button>("SubmitButton");
         _SubmitButton.IsEnabled = false;
+        _Podsumowanie = this.FindControl<TextBlock>("SummaryTextBlock");
+        
         _TypPrzesylki.SelectionChanged += (_, _) => UpdatePrice();
         
         _Waga.KeyUp += (_, _) => UpdatePrice();
@@ -35,6 +39,7 @@ public partial class MainWindow : Window
         _Szerokosc.KeyUp += (_, _) => UpdatePrice();
         _Glebokosc.KeyUp += (_, _) => UpdatePrice();
         _Ekspres.Click += (_, _) => UpdatePrice();
+        _SubmitButton.Click += SubmitButton_Click;
         void UpdatePrice()
         {
             if (int.TryParse(_Waga.Text, out int weight) &&
@@ -62,6 +67,36 @@ public partial class MainWindow : Window
             {
                 _SubmitButton.IsEnabled = false;
                 _Cena.Text = "";
+            }
+        }
+
+        void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(_Waga.Text, out int weight) &&
+                int.TryParse(_Wysokosc.Text, out int h) &&
+                int.TryParse(_Szerokosc.Text, out int s) &&
+                int.TryParse(_Glebokosc.Text, out int g))
+            {
+                bool ekspres = _Ekspres.IsChecked == true;
+                int typ = _TypPrzesylki.SelectedIndex;
+
+                int price = _calculator.CalculatePrice(weight, h, s, g, ekspres, typ);
+
+                string typText = "";
+
+                if (typ == 0) typText = "Standardowa";
+                if (typ == 1) typText = "Ostrożnie (szkło)";
+                if (typ == 2) typText = "Paleta";
+
+                string ekspresText = ekspres ? "Tak" : "Nie";
+
+                _Podsumowanie.Text =
+                    $"Podsumowanie:\n" +
+                    $"Wymiary: {h} x {s} x {g} cm\n" +
+                    $"Waga: {weight} kg\n" +
+                    $"Typ przesyłki: {typText}\n" +
+                    $"Ekspres: {ekspresText}\n" +
+                    $"Cena końcowa: {price} zł";
             }
         }
     }
