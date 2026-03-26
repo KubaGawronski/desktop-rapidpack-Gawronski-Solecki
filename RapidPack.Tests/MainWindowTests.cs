914911347ce8a579b93867be485e1f26114f6fef
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-03-26 20:57:49 +0100

    Add xUnit test for weight

--- RapidPack.Tests/MainWindowTests.cs
+++ RapidPack.Tests/MainWindowTests.cs
@@ -1,13 +1,17 @@
 ﻿using Avalonia.Headless.XUnit;
-
+using RapidPack.Services;
 namespace RapidPack.Tests;
 
 public class MainWindowTests
 {
    [AvaloniaFact]
-   public void CreateWindow_ShouldCreateANewWindow()
+   public void CzyWagaWiekszaOd30ZwracaBlad()
    {
-      var window = new MainWindow();
-      Assert.NotNull(window);
+      var calculator = new ParcelCalculator();
+      
+      Assert.Throws<ArgumentException>(() =>
+         calculator.CalculatePrice(35, 10, 10, 10, false, 0)
+      );
    }
+   
 }
